7d130f0d348e698a29d181e58f8fdc974b2cc1bc
Grace Gaciuki <78306838+gaciuki2402@users.noreply.github.com>
2024-06-12 04:19:58 +0300

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -148,6 +148,7 @@ def main():
                 background-image: url("data:image/png;base64,{encoded_string}");
                 background-size: cover;
                 background-position: center;
+                color: grey
             }}
             </style>
             """,
